a2a85418758b71dc703db4abd9de53245ecd6580
23NateK <n.kimber22@gmail.com>
2023-03-01 12:46:43 -0600

    Finished Date and Worked on TimeV1

--- Date.java
+++ Date.java
@@ -1,5 +1,45 @@
 public class Date {
-    public static void main(String[] args) {
-        
+    private int month;
+    private int day;
+    private int year;
+
+
+    public Date(int intMonth, int intDay, int intYear){
+        month=intMonth; 
+        day=intDay;
+        year=intYear;
     }
+
+    public int getMonth(){
+        return month;
+    }
+    public int getDay(){
+        return day;
+    }
+    public int getYear(){
+        return year;
+    }
+
+    public String toString(){
+        String result="";
+        if (month<10){
+            result+="0"+month+"/";
+        }else{
+        result+=month+"/";
+        }
+        if(day<10){
+            result+="0"+day+"/";
+        }else{
+        result+=day+"/";
+        }
+        result+=year;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Date d1;
+        d1 = new Date(4,9,1999);
+        System.out.println(d1.toString());
+    }
+
 }
--- TimeV1.java
+++ TimeV1.java
@@ -1,5 +1,28 @@
 public class TimeV1 {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public TimeV1(int intHours, int intMinutes, int intSeconds){
+        hours=intHours;
+        minutes=intMinutes;
+        seconds=intSeconds;
+    }
+    public int getHours(){
+        return hours;
+    }
+    public int getMinutes(){
+        return minutes;
+    }
+    public int getSeconds(){
+        return seconds;
+    }
+
+
+
     public static void main(String[] args) {
-        
+        TimeV1 time = new TimeV1(7,02,23);
+        System.out.println(time.toString());
+
     }
 }
